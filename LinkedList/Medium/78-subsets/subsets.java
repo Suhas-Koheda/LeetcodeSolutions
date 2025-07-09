75a03f039288a82f8682eba6e6fdba53a5473550
HaasBroo <sharmasuhas450@gmail.com>
2025-07-09 08:43:23 +0530

    Time: 1 ms (50.71%) | Memory: 42.8 MB (46.40%) - LeetSync

---
+++ LinkedList/Medium/78-subsets/subsets.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> l=new ArrayList<>();
+        solve(0,nums,new ArrayList(),l);
+        System.out.println(nums.length);
+        return l;
+    }
+    void solve(int i,int[] nums,List<Integer> f,List<List<Integer>> l){
+       if(i==nums.length){
+        l.add(new ArrayList<>(f));
+        return;
+       }
+       f.add(nums[i]);
+       solve(i+1,nums,f,l);
+       f.remove(f.size()-1);
+       solve(i+1,nums,f,l);
+    }
+}
