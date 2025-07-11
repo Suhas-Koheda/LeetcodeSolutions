70171410cd6e637f5a8d4737dcee1ff663d7f1e3
HaasBroo <sharmasuhas450@gmail.com>
2025-07-11 17:27:23 +0530

    Time: 38 ms (2.40%) | Memory: 44.8 MB (6.77%) - LeetSync

---
+++ LinkedList/Medium/216-combination-sum-iii/combination-sum-iii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> l=new ArrayList<>();
+        solve(l,new ArrayList<>(),1,k,n);
+        return l;
+    }
+    void solve(List<List<Integer>> l,List<Integer>ds,int i,int k,int n){
+            System.out.println(ds+ " "+n+" "+k);
+        if(n==0 && ds.size()==k){
+                l.add(new ArrayList<>(ds));
+            return;
+        }
+        if (ds.size() > k || n < 0 || i > 9) {
+            return;
+        }
+        ds.add(i);
+        solve(l,ds,i+1,k,n-i);
+        ds.remove(ds.size()-1);
+        solve(l,ds,i+1,k,n);
+    }
+}
