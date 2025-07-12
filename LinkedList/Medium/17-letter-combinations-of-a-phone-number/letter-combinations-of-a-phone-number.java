479e73905820e98830fca96645cd5a0bb97e9aa4
HaasBroo <sharmasuhas450@gmail.com>
2025-07-12 05:54:57 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (38.72%) - LeetSync

---
+++ LinkedList/Medium/17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        if(digits.length()==0){
+            return new ArrayList<>();
+        }
+        List<String> l=new ArrayList<>();
+        String[] map = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        solve(digits,map,l,new StringBuilder(),0);
+        return l;
+    }
+
+    void solve(String digits,String[] map,List<String> l,StringBuilder ds,int i){
+        if(i==digits.length()){
+            l.add(new String(ds.toString()));
+            return;
+        }
+        int x=digits.charAt(i)-'0';
+        String d=map[x];
+        for(int j=0;j<d.length();j++){
+            ds.append(d.charAt(j));
+            solve(digits,map,l,ds,i+1);
+            ds.deleteCharAt(ds.length()-1);
+        }
+    }
+}
