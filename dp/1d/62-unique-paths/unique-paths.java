177274221681254626bef0cdace0a09340c3ed3c
HaasBroo <sharmasuhas450@gmail.com>
2025-08-30 22:49:07 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (47.30%) - LeetSync

--- dp/1d/62-unique-paths/unique-paths.java
+++ dp/1d/62-unique-paths/unique-paths.java
@@ -3,24 +3,18 @@ class Solution {
         int dp[][]=new int[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                dp[i][j]=-1;
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                    continue;
+                }
+                int up=0;
+                int left=0;
+                if(i>0){up=dp[i-1][j];}
+                if(j>0){left=dp[i][j-1];}
+                dp[i][j] = up+left;
             }
         }
-        return func(m-1,n-1,dp);
-    }
-    public int func(int row,int col,int dp[][]){
-        if(row==0 && col==0){
-            return 1;
-        }
-        if(row<0 || col<0 ){
-            return 0;
-        }
-        if(dp[row][col]!=-1){
-            return dp[row][col];
-        }
-        int up=func(row-1,col,dp);
-        int left=func(row,col-1,dp);
-        dp[row][col] = up+left;
-        return dp[row][col];
+
+        return dp[m-1][n-1];
     }
 }
