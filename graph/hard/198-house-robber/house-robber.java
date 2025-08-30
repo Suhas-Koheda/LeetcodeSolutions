79c7fdd133d58e929b0e4b9eb3a5634a45136515
HaasBroo <sharmasuhas450@gmail.com>
2025-08-30 16:09:13 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (34.80%) - LeetSync

--- graph/hard/198-house-robber/house-robber.java
+++ graph/hard/198-house-robber/house-robber.java
@@ -1,16 +1,18 @@
 class Solution {
     public int rob(int[] nums) {
-        int arr[]=new int[nums.length];
-        arr[0]=nums[0];
-        int max=-999999;
+        int prev=nums[0];
+        int prev2=0;
+        int curr=prev;
         for(int i=1;i<nums.length;i++){
             int take=nums[i];
             if(i>1){
-                take+=arr[i-2];
+                take+=prev2;
             }
-            int nottake=arr[i-1];
-            arr[i]=Math.max(take,nottake);
+            int nottake=prev;
+            curr=Math.max(take,nottake);
+            prev2=prev;
+            prev=curr;
         }
-        return arr[nums.length-1];
+        return curr;
     }
 }
