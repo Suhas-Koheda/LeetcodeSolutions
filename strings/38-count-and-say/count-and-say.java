e536ae6d04c2b2d8b912363e3163d0feee10adeb
HaasBroo <sharmasuhas450@gmail.com>
2025-10-18 22:31:21 +0530

    Time: 12 ms (16.63%) | Memory: 44.7 MB (16.11%) - LeetSync

---
+++ strings/38-count-and-say/count-and-say.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String countAndSay(int n) {
+        return rle(n);
+    }
+    String rle(int n){
+        if(n==1) return "1";
+        String s=rle(n-1);
+        int count=0;
+        char prev=s.charAt(0);
+        StringBuffer s1=new StringBuffer();
+        for(char i:s.toCharArray()){
+            if (i == prev) {
+                count++;
+            } 
+            else{
+                s1.append(Integer.toString(count)+prev);
+                count=1;
+                prev=i;
+            }
+        }
+        s1.append(count).append(prev);
+        return s1.toString();
+    }
+}
