3dad7327a37e38d40f64af36207633a4777da3c3
HaasBroo <sharmasuhas450@gmail.com>
2025-10-24 18:15:33 +0530

    Time: 476 ms (17.69%) | Memory: 44.8 MB (23.08%) - LeetSync

---
+++ strings/2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int nextBeautifulNumber(int n) {
+        n=n+1;
+        while (check(n) == false && n < 10000000) {
+            n++;
+        }
+        return n;
+    }
+
+    boolean check(int n) {
+        int freq[] = new int[10];
+        for (char i : Integer.toString(n).toCharArray()) {
+            int x = Integer.parseInt(Character.toString(i));
+            freq[x]++;
+        }
+        for (int i = 1; i < 10; i++) {
+            if (freq[i] > 0 && i != freq[i])
+                return false;
+        }
+        if (freq[0] > 0) {
+            return false;
+        }
+        return true;
+    }
+}
