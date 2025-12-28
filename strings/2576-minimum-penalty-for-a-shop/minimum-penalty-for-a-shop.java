0e468c51994b9ff0487b0fda08958d6f255bc77c
HaasBroo <sharmasuhas450@gmail.com>
2025-12-28 12:50:03 +0530

    Time: 8 ms (86.92%) | Memory: 47.2 MB (24.16%) - LeetSync

---
+++ strings/2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public int bestClosingTime(String customers) {
+        int lowestScore = 0;
+        int score = 0;
+        int bestHour = 0;
+
+        for (int idx = 0; idx < customers.length(); idx++) {
+            char c = customers.charAt(idx);
+
+            if (c == 'Y') {
+                score--;
+            } else {
+                score++;
+            }
+
+            if (score < lowestScore) {
+                lowestScore = score;
+                bestHour = idx + 1;
+            }
+        }
+
+        return bestHour;
+    }
+}
