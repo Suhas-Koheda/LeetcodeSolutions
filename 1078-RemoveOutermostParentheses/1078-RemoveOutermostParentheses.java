53cca8edd0bddc818cda137d319d8494423179ac
HaasBroo <sharmasuhas450@gmail.com>
2026-01-02 10:08:40 +0530

    Create 1078-RemoveOutermostParentheses

---
+++ 1078-RemoveOutermostParentheses/1078-RemoveOutermostParentheses.java
@@ -0,0 +1,23 @@
+// Last updated: 02/01/2026, 10:08:40
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder result = new StringBuilder();
+        int open = 0;
+        
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                if (open !=0) {
+                    result.append(ch);
+                }
+                open++;
+            } else if (ch == ')') {
+                open--;
+                if (open!= 0) {
+                    result.append(ch);
+                }
+            }
+        }
+        
+        return result.toString();
+    }
+}
