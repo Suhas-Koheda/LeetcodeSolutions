a4f323730640d96812f76c4b8ae22bbd874f7ed1
HaasBroo <sharmasuhas450@gmail.com>
2026-01-02 10:10:13 +0530

    Create 90-SubsetsIi

---
+++ 90-SubsetsIi/90-SubsetsIi.java
@@ -0,0 +1,21 @@
+// Last updated: 02/01/2026, 10:10:13
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> l =new ArrayList<>();
+        solve(nums,0,l,new ArrayList<>());
+        return l;
+    }
+    void solve(int[] nums,int i, List<List<Integer>> l,List<Integer> ds){
+        if(i==nums.length){
+            if( !l.contains(ds)){
+            l.add(new ArrayList<>(ds));
+            }
+            return;
+        }
+        ds.add(nums[i]);
+        solve(nums,i+1,l,ds);
+        ds.remove(ds.size()-1);
+        solve(nums,i+1,l,ds);
+    }
+}
