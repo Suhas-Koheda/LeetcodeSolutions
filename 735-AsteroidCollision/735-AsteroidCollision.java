8affcf9fd499fc7870be1fe96e904cdb2b99c339
HaasBroo <sharmasuhas450@gmail.com>
2026-01-02 10:09:08 +0530

    Create 735-AsteroidCollision

---
+++ 735-AsteroidCollision/735-AsteroidCollision.java
@@ -0,0 +1,30 @@
+// Last updated: 02/01/2026, 10:09:07
+class Solution {
+    public int[] asteroidCollision(int[] arr) {
+        // Arrays.sort(asteroids);
+        Stack<Integer> s=new Stack<>();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>0){
+                s.push(arr[i]);
+            }
+            else{
+                while(!s.isEmpty() && s.peek()>0 && s.peek()<Math.abs(arr[i])){
+                    s.pop();
+                }
+                if(!s.isEmpty()&&s.peek()==Math.abs(arr[i])){
+                    s.pop();
+                }
+                else if(s.isEmpty()||s.peek()<0){
+                    s.push(arr[i]);
+                }
+            }
+        }
+        int ans[]=new int[s.size()];
+        int k=s.size()-1;
+        System.out.println(k);
+        while(!s.isEmpty()){
+            ans[k--]=s.pop();
+        }
+        return ans;
+    }
+}
