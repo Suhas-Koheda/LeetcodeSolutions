70cf093b898f6627225cfd12da4e813e6ab088a4
HaasBroo <sharmasuhas450@gmail.com>
2026-01-02 10:06:40 +0530

    Create 3797-DesignSpreadsheet

---
+++ 3797-DesignSpreadsheet/3797-DesignSpreadsheet.java
@@ -0,0 +1,41 @@
+// Last updated: 02/01/2026, 10:06:39
+class Spreadsheet {
+    HashMap<String,Integer>p;
+
+    public Spreadsheet(int rows) {
+        p = new HashMap<>();
+    }
+
+    public void setCell(String cell, int value) {
+        p.put(cell,value);
+    }
+
+    public void resetCell(String cell) {
+        p.remove(cell);
+    }
+
+    private int get(String s) {
+        if (s.charAt(0) > '9') {
+            return p.getOrDefault(s,0);
+        } else {
+            return Integer.parseInt(s);
+        }
+    }
+
+    public int getValue(String formula) {
+        String qsns[] = formula.substring(1).split("\\+");
+        int ans = 0;
+        for (String i : qsns) {
+            ans += get(i);
+        }
+        return ans;
+    }
+}
+
+/**
+ * Your Spreadsheet object will be instantiated and called as such:
+ * Spreadsheet obj = new Spreadsheet(rows);
+ * obj.setCell(cell,value);
+ * obj.resetCell(cell);
+ * int param_3 = obj.getValue(formula);
+ */
