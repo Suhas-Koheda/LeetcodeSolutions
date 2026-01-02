ab121ee2a865fd6cbfadbc7170f89d850ab62cc6
HaasBroo <sharmasuhas450@gmail.com>
2026-01-02 10:11:14 +0530

    Create 13-RomanToInteger

---
+++ 13-RomanToInteger/13-RomanToInteger.py
@@ -0,0 +1,14 @@
+# Last updated: 02/01/2026, 10:11:14
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        s = s.replace("IV", "IIII").replace("IX", "VIIII").replace("XL", "XXXX").replace("XC", "LXXXX").replace("CD", "CCCC").replace("CM", "DCCCC")
+        roman_to_integer = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+        }
+        return sum(map(lambda x: roman_to_integer[x], s))
